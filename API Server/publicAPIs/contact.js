8b0bf5518131f94841ed50d60f0d8556eebb10ca
Mitchell Adair <mitchwadair@gmail.com>
2020-06-10 17:03:51 -0400

    add license headers to files without them

--- API Server/publicAPIs/contact.js
+++ API Server/publicAPIs/contact.js
@@ -1,3 +1,8 @@
+// Copyright (c) 2020 Mitchell Adair
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 const nodemailer = require('nodemailer');
 
 const post = (req, res) => {
