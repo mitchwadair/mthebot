16856de560831db811725484602762f8e9aff3bc
Mitchell Adair <mitchwadair@gmail.com>
2022-04-05 19:51:01 -0400

    #57: async/await in chats API

--- API Server/privateAPIs/chats.js
+++ API Server/privateAPIs/chats.js
@@ -5,52 +5,40 @@
 
 const DBService = require("../../dbservice");
 
-const get = (req, res) => {
-    const channel = req.params.channel;
-    DBService.getChannel(channel)
-        .then((data) => {
-            if (data) res.status(200).send(data.enabled.toString());
-            else res.status(404).send(`Channel ${encodeURIComponent(channel)} not found`);
-        })
-        .catch((err) => {
-            res.status(500).send(encodeURIComponent(err.toString()));
-        });
+const get = async (req, res) => {
+    const { channel } = req.params;
+    try {
+        const data = await DBService.getChannel(channel);
+        if (data) {
+            res.status(200).send(data.enabled.toString());
+        } else {
+            res.status(404).send(`Channel ${channel} not found`);
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 };
 
-const post = (actions, req, res) => {
-    const channel = req.params.channel;
-    DBService.enableChannel(channel)
-        .then(() => {
-            actions
-                .joinChannel(channel)
-                .then(() => {
-                    res.status(200).send(`Bot set to enabled for channel ${encodeURIComponent(channel)}`);
-                })
-                .catch((err) => {
-                    res.status(500).send(encodeURIComponent(err.toString()));
-                });
-        })
-        .catch((err) => {
-            res.status(500).send(encodeURIComponent(err.toString()));
-        });
+const post = async (actions, req, res) => {
+    const { channel } = req.params;
+    try {
+        await DBService.enableChannel(channel);
+        await actions.joinChannel(channel);
+        res.status(200).send(`Bot set to enabled for channel ${channel}`);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 };
 
-const remove = (actions, req, res) => {
-    const channel = req.params.channel;
-    DBService.disableChannel(channel)
-        .then(() => {
-            actions
-                .leaveChannel(channel)
-                .then(() => {
-                    res.status(200).send(`Bot set to disabled for channel ${encodeURIComponent(channel)}`);
-                })
-                .catch((err) => {
-                    res.status(500).send(encodeURIComponent(err.toString()));
-                });
-        })
-        .catch((err) => {
-            res.status(500).send(encodeURIComponent(err.toString()));
-        });
+const remove = async (actions, req, res) => {
+    const { channel } = req.params;
+    try {
+        await DBService.disableChannel(channel);
+        await actions.leaveChannel(channel);
+        res.status(200).send(`Bot set to disabled for channel ${channel}`);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 };
 
 module.exports = {
